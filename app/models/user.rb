5f21124bd7a3de00590225ac43b70073082cdb8a
Xavi122323 <xavier.ramos@udla.edu.ec>
2023-11-13 11:23:49 -0500

    User generate token, aplication controller token

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :recoverable, :registerable, :trackable, :lockable
+
+  enum role: [:user, :admin], _default: :user
+
+  def generate_jwt
+    JWT.encode({id: id, role: role, exp: 60.days.from_now.to_i}, Rails.application.secrets.secret_key_base)
+  end
 end
