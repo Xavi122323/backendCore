a3592a386e5ad36178af4123bb09f9716d030d17
Xavi122323 <xavier.ramos@udla.edu.ec>
2023-11-13 11:10:58 -0500

    preparacion de rutas y gemas

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,13 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  # root "articles#index" 
+  
+  namespace :api, defaults: {format: :json} do
+    namespace :v1 do
+      root "servidor#index"
+      resources :servidores, only:[:index, :show, :create, :update, :destroy]
+      
+    end
+  end
 end
