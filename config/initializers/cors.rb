7d6fc3ccd12d770a7bad1405965df29a04aeeff1
Xavi122323 <xavier.ramos@udla.edu.ec>
2023-11-17 01:24:45 -0500

    origins update v2

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,7 +8,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
       origins "http://localhost:4200"
-      origins "https://corebackend.onrender.com"
+      origins "https://frontend-core-7tveg0onm-xavi122323s-projects.vercel.app"
  
       resource "*",
         headers: :any,
