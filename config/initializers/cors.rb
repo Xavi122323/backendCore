5256938abde845d4fe099035247fde21af3d4f60
Xavi122323 <xavier.ramos@udla.edu.ec>
2023-11-17 01:21:14 -0500

    origins update

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,6 +8,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
       origins "http://localhost:4200"
+      origins "https://corebackend.onrender.com"
  
       resource "*",
         headers: :any,
