13ac77ccba13f038aeed2cf2029069feff6e9c49
Xavi122323 <xavier.ramos@udla.edu.ec>
2024-05-26 20:46:32 -0500

    Cors update

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,10 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins "http://localhost:4200", "frontend-core-blue.vercel.app", "frontend-core-git-master-xavi122323s-projects.vercel.app", "frontend-core-q44aeqw2q-xavi122323s-projects.vercel.app"
+      origins "http://localhost:4200", "frontend-core-blue.vercel.app", 
+      "frontend-core-git-master-xavi122323s-projects.vercel.app", 
+      "frontend-core-q44aeqw2q-xavi122323s-projects.vercel.app",
+      "https://adrianbedon.github.io/front-react/"
  
       resource "*",
         headers: :any,
