7f3e1c1765dcd59171df55adf84b82098cdf0b4d
Xavi122323 <xavier.ramos@udla.edu.ec>
2024-07-11 21:38:31 -0500

    json v2

--- config/initializers/google_cloud_kms.rb
+++ config/initializers/google_cloud_kms.rb
@@ -1,6 +1,7 @@
 # config/initializers/google_cloud_kms.rb
 require "google/cloud/kms"
 
+credentials_json = ENV['GOOGLE_APPLICATION_CREDENTIALS_JSON']
 Google::Cloud::Kms.configure do |config|
-  config.credentials = JSON.parse(ENV['GOOGLE_APPLICATION_CREDENTIALS_JSON'])
+  config.credentials = JSON.parse(credentials_json)
 end
