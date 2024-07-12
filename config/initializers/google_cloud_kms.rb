c48c91a0a03466619cfedb0d22151318b41b761b
Xavi122323 <xavier.ramos@udla.edu.ec>
2024-07-11 21:09:28 -0500

    google_cloud_kms update

--- config/initializers/google_cloud_kms.rb
+++ config/initializers/google_cloud_kms.rb
@@ -1,2 +1,6 @@
 # config/initializers/google_cloud_kms.rb
 require "google/cloud/kms"
+
+Google::Cloud::Kms.configure do |config|
+  config.credentials = JSON.parse(ENV['GOOGLE_APPLICATION_CREDENTIALS'])
+end
